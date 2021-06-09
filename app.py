b7cb30ccd644248ff42bf75e3c2b768b7ec6e623
sarmini1 <spencerarmini@gmail.com>
2021-06-08 17:54:40 -0700

    building out upload functionality

--- app.py
+++ app.py
@@ -1,57 +1,94 @@
 """Flask app for Pixly Backend"""
-from flask import Flask, url_for, render_template, redirect, flash, jsonify, request
-from models import Photo, connect_db
+import os
+from flask import Flask, url_for, render_template, jsonify, request
+from werkzeug.utils import secure_filename
+from project_secrets import SECRET_KEY, AWS_ACCESS_KEY, AWS_SECRET_KEY, BUCKET_NAME
+from PIL import Image
+from PIL.ExifTags import TAGS
+import boto3
+# from models import Photo, connect_db
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = "secret"
+client = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_KEY )
+
+app.config['SECRET_KEY'] = SECRET_KEY
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///cupcakes"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
 
-@app.route('/api/photos')
-def list_all_cupcakes():
-    """
-    Return JSON 
-    {photos: [{id, flavor, size, rating, image}, ...]}.
-    """
+@app.route("/")
+def display_new_photo_form():
+    """"""
 
-    photos = Photo.query.all()
-    serialized = [p.serialize() for p in photos]
+    return render_template("dumby.html")
 
-    return jsonify(photos=serialized)
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload_file():
+    """"""
+
+    if request.method == 'POST':
+        f = request.files['file']
+        print("f is", f)
+
+        filename = secure_filename(f.filename)
+        f.save(os.path.join(filename))
+        img = Image.open(filename)
+        client.upload_file(filename, BUCKET_NAME, "something", ExtraArgs={"ACL":"public-read"})
+        os.remove(filename)
+
+        exif_values = {}
+        for tag, value in img.getexif().items():
+            if tag in TAGS:
+                exif_values[TAGS[tag]] = value
+        return jsonify(exif_values)
+
+# loop over all tags in list of items from img.
+
+# @app.route('/api/photos')
+# def list_all_cupcakes():
+#     """
+#     Return JSON 
+#     {photos: [{id, flavor, size, rating, image}, ...]}.
+#     """
+
+#     photos = Photo.query.all()
+#     serialized = [p.serialize() for p in photos]
+
+#     return jsonify(photos=serialized)
 
     
-@app.route("/api/photos", methods=["POST"])
-def create_photo():
-    """
-    THIS IS EXAMPLE: 
-    Returns JSON 
+# @app.route("/api/photos", methods=["POST"])
+# def create_photo():
+#     """
+#     THIS IS EXAMPLE: 
+#     Returns JSON 
     
-    {cupcake: {id, flavor, size, rating, image}}.
-    """
+#     {cupcake: {id, flavor, size, rating, image}}.
+#     """
 
-    flavor = request.json["flavor"]
-    size = request.json["size"]
-    rating = request.json["rating"]
-    # image = request.json["image"] OR default
-    if not request.json["image"]:
-        image = 'https://tinyurl.com/demo-cupcake'
-    else:
-        image = request.json["image"]
+#     flavor = request.json["flavor"]
+#     size = request.json["size"]
+#     rating = request.json["rating"]
+#     # image = request.json["image"] OR default
+#     if not request.json["image"]:
+#         image = 'https://tinyurl.com/demo-cupcake'
+#     else:
+#         image = request.json["image"]
 
-    cupcake = Cupcake(
-        flavor=flavor, 
-        size=size,
-        rating=rating,
-        image=image
-    )
+#     cupcake = Cupcake(
+#         flavor=flavor, 
+#         size=size,
+#         rating=rating,
+#         image=image
+#     )
 
-    db.session.add(cupcake)
-    db.session.commit()
+#     db.session.add(cupcake)
+#     db.session.commit()
 
-    serialized = cupcake.serialize()
+#     serialized = cupcake.serialize()
 
-    # Return w/status code 201 --- return tuple (json, status)
-    return (jsonify(cupcake=serialized), 201)
+#     # Return w/status code 201 --- return tuple (json, status)
+#     return (jsonify(cupcake=serialized), 201)
--- models.py
+++ models.py
@@ -1,39 +1,39 @@
-"""Models for Pixly app."""
-from flask_sqlalchemy import SQLAlchemy
+# """Models for Pixly app."""
+# from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+# db = SQLAlchemy()
 
-def connect_db(app):
-    """Connect this database to provided Flask app.
-    You should call this in your Flask app.
-    """
-    db.app = app
-    db.init_app(app)
+# def connect_db(app):
+#     """Connect this database to provided Flask app.
+#     You should call this in your Flask app.
+#     """
+#     db.app = app
+#     db.init_app(app)
 
 
-class Photo(db.Model):
-    "data on a photo"
+# class Photo(db.Model):
+#     "data on a photo"
 
-    __tablename__ = "photos"
+#     __tablename__ = "photos"
 
-    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    location = db.Column(db.String(100), nullable=False)
-    # colorData= db.Column(db.String(100), nullable=False)
-    height = db.Column(db.String(100), nullable=False)
-    width = db.Column(db.String(100), nullable=False)
-    image_path = db.Column(db.String(100), nullable=False)
+#     id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+#     location = db.Column(db.String(100), nullable=False)
+#     # colorData= db.Column(db.String(100), nullable=False)
+#     height = db.Column(db.String(100), nullable=False)
+#     width = db.Column(db.String(100), nullable=False)
+#     image_path = db.Column(db.String(100), nullable=False)
 
-    def serialize(self):
-        """Serialize to dictionary."""
+#     def serialize(self):
+#         """Serialize to dictionary."""
 
-        return {
-            "id": self.id,
-            "location": self.flavor,
-            "height": self.size,
-            "width": self.rating,
-            "image_path": self.image,
-        }
+#         return {
+#             "id": self.id,
+#             "location": self.flavor,
+#             "height": self.size,
+#             "width": self.rating,
+#             "image_path": self.image,
+#         }
 
-    def get_photo_from_aws(self):
-        return
+#     def get_photo_from_aws(self):
+#         return
         
