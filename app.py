1bb245557806d379375610f34f4b3eca1fd5373d
Nate Deakers <ndeakers@gmail.com>
2021-06-09 11:34:08 -0700

    uploaded images

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Flask app for Pixly Backend"""
 import os
-from flask import Flask, url_for, render_template, jsonify, request
+from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 from project_secrets import SECRET_KEY, AWS_ACCESS_KEY, AWS_SECRET_KEY, BUCKET_NAME
 from PIL import Image
@@ -16,6 +16,8 @@ app.config['SECRET_KEY'] = SECRET_KEY
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///cupcakes"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+UPLOAD_FOLDER = './UPLOAD_FOLDER'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 @app.route("/")
@@ -26,7 +28,7 @@ def display_new_photo_form():
 
 
 @app.route('/uploader', methods=['GET', 'POST'])
-def upload_file():
+def upload_photo():
     """"""
 
     if request.method == 'POST':
@@ -34,16 +36,45 @@ def upload_file():
         print("f is", f)
 
         filename = secure_filename(f.filename)
-        f.save(os.path.join(filename))
-        img = Image.open(filename)
-        client.upload_file(filename, BUCKET_NAME, "something", ExtraArgs={"ACL":"public-read"})
-        os.remove(filename)
+        # f.save(os.path.join(filename))
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
+        img = Image.open("./UPLOAD_FOLDER/jpegsystems-home.jpeg")
+        img_color = img.convert("RGBA")
+        w, h = img_color.size
+        cnt = 0
+        for px in img_color.getdata():
+            img_color.putpixel((int(cnt % w), int(cnt / w)), (0, 0, 0, px[3]))
+            cnt += 1  
+        print("what is image_color", img_color.getdata())
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], "newImage.jpeg"))
+        return render_template("image.html")
+
+
+        client.upload_file(filename, BUCKET_NAME, "something", ExtraArgs={"ACL":"public-read"})
+
+
+
+        # os.remove(filename)
+
+        # extract EXIF data
+        exifdata = img.getexif()
         exif_values = {}
-        for tag, value in img.getexif().items():
-            if tag in TAGS:
-                exif_values[TAGS[tag]] = value
-        return jsonify(exif_values)
+        # iterating over all EXIF data fields
+        for tag_id in exifdata:
+            # get the tag name, instead of human unreadable tag id
+            # tag = TAGS.get(tag_id, tag_id)
+            data = exifdata.get(tag_id)
+            exif_values[tag_id] = data
+            print("exif_values", exif_values)
+        return "all done"
+                
+        # exif_values = {}
+        # for tag in img.getexif().items:
+        #     print("exifValues", tag)
+        #     # if tag in TAGS:
+        #     #     exif_values[TAGS[tag]] = value
+        # return jsonify(exif_values)
 
 # loop over all tags in list of items from img.
 
